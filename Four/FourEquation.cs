893606d7e5679dec1ae9d362739d52366b027f64
McLogicmaster69 <logicmaster5823@gmail.com>
2024-01-12 01:02:38 +0000

    initial

---
+++ Four/FourEquation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Four
+{
+    public class FourEquation
+    {
+        public float Equate()
+        {
+            return 0f;
+        }
+    }
+}
---
+++ Four/FourLibrary.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Four
+{
+    public class FourLibrary
+    {
+        public List<FourSolution> Solutions = new List<FourSolution>();
+
+        public void AddEquation(FourEquation equation)
+        {
+            float value = equation.Equate();
+            if (value % 1f == 0f)
+                AddEquation(equation, (int)value);
+        }
+
+        private void AddEquation(FourEquation equation, int value)
+        {
+            if(Solutions.Count == 0)
+            {
+                FourSolution solution = new FourSolution();
+                solution.Equations.Add(equation);
+                solution.Value = value;
+                Solutions.Add(solution);
+            }
+
+
+        }
+
+        private int BinarySort(int value)
+        {
+            int lower = 0;
+            int upper = Solutions.Count - 1;
+
+            while(lower < upper)
+            {
+                int middle = (lower + upper) / 2;
+                if (Solutions[middle].Value == value)
+                    return middle;
+                else if (Solutions[middle].Value < value)
+                    lower = middle + 1;
+                else
+                    upper = middle - 1;
+            }
+
+            if (lower >= Solutions.Count)
+            {
+                FourSolution solution = new FourSolution();
+                solution.Value = value;
+                Solutions.Add(solution);
+                return Solutions.Count - 1;
+            }
+
+            if(upper < 0)
+            {
+                FourSolution solution = new FourSolution();
+                solution.Value = value;
+                Solutions.Insert(0, solution);
+                return 0;
+            }
+            else
+            {
+                FourSolution solution = new FourSolution();
+                solution.Value = value;
+                Solutions.Insert(lower, solution);
+                return lower;
+            }
+        }
+    }
+}
